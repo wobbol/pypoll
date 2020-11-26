2d423fc24138783f2484a19135f7cbcdc2bddba5
N.F </dev/null>
2020-11-26 03:01:54 -0600

    Flesh out first iteration of database schema
    Session cookies are being used to keep clients honest about their session.
    The database schema in a seperate file makes working outside python easier.
    It is important that the PRIMARY KEY of each table be of type INTERGER.
    Otherwise, sqlite3 fails to recognize that it is a PRIMARY KEY and assigns NULL.
    
    Without 'PRAGMA foreign_keys = 1' sqlite3 does not enforce foreign_keys constraints.
    ON DELETE RESTRICT should keep the database valid even if an attempt is made to delete the referenced parent table row.
    ON UPDATE CASCADE should update all the references to a row's primary key if the parent table is changed.

--- pypoll.py
+++ pypoll.py
@@ -1,6 +1,9 @@
 import sqlite3
 import os
+import time
 from flask import Flask, session, redirect, url_for, request, g
+from socket import inet_pton, inet_ntop
+from struct import unpack, pack
 app = Flask(__name__)
 app.secret_key = os.urandom(16) # TODO: store this in a file
 
@@ -14,6 +17,26 @@ navigation = '''
         <li> <a href="/select">/select</a> </li>
     </ul>
 '''
+def make_unique_id():
+    #TODO: report birthday paradox numbers here
+    return os.urandom(8)
+
+def ip2bytes(ip):
+    try:
+        return inet_pton(AF_INET, ip)
+    except OSError:
+        try:
+            return inet_pton(AF_INET6, ip)
+        except OSError:
+            return None
+def bytes2ip(ip_bytes):
+    try:
+        return inet_ntop(AF_INET, ip_bytes)
+    except (ValueError, OSError):
+        try:
+            return inet_ntop(AF_INET6, ip_bytes)
+        except (ValueError, OSError):
+            return None
 
 #isolation_level=None leaves sqlite3 in autcommit mode
 def get_db():
@@ -21,6 +44,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect(database, isolation_level=None)
         db.row_factory = sqlite3.Row
+        db.execute('PRAGMA foreign_keys = 1')
     return db
 
 def query_db(query, args=()):
@@ -41,7 +65,14 @@ def poll_test():
     if request.method == 'POST':
         if 'unique_id' in session:
             return 'thonk'
-        session['unique_id'] = os.urandom(8)
+        session['unique_id'] = make_unique_id()
+        now = time.time()
+        conn = get_db()
+        c = conn.cursor()
+        values = (now, session['unique_id'], request.remote_addr)
+        c.execute("INSERT INTO users (date, cookie, ip) VALUES (?, ?, ?)", values)
+
+
         return redirect(url_for('home'))
     return '''
     <form method="post">
@@ -55,14 +86,8 @@ def poll_test():
 def create_test():
     conn = get_db()
     #try:
-    conn.execute('''
-    CREATE TABLE user (
-            id     integer  NOT NULL PRIMARY KEY ASC,
-            date   text NOT NULL,
-            cookie text NOT NULL,
-            ip     text NOT NULL
-    )
-    ''')
+    with app.open_resource('tables.sql', mode='r') as t:
+        conn.cursor().executescript(t.read())
     #except sqlite3.OperationalError:
     #    print('Database exists already')
     #    raise
@@ -73,13 +98,13 @@ def create_test():
 def insert_test():
     conn = get_db()
     c = conn.cursor()
-    values = ('20-01-2020', 'cookie', '127.0.0.1')
-    c.execute("INSERT INTO user (date, cookie, ip) VALUES (?, ?, ?)", values)
+    values = (time.time(), make_unique_id(), '127.0.0.1')
+    c.execute("INSERT INTO users (date, cookie, ip) VALUES (?, ?, ?)", values)
     return navigation + '<p>done</p>'
 
 @app.route('/sel')
 def sel():
-    for trans in query_db('SELECT * from user WHERE ip="127.0.0.1"'):
+    for trans in query_db('SELECT * from users WHERE ip="127.0.0.1"'):
         print(trans['date'])
         print(str(tuple(trans)))
     return 'ha'
@@ -91,8 +116,11 @@ def select_test():
     c = conn.cursor()
     t = ('127.0.0.1',)
     ret = ""
-    for row in c.execute('SELECT * FROM user WHERE ip=?', t):
-        ret += str(tuple(row))
+    for row in c.execute('SELECT * FROM users WHERE ip=?', t):
+        ret += str(row['id']) + ", "
+        ret += time.asctime(time.localtime(row['date'])) + ", "
+        ret += "0x" + row['cookie'].hex() + ", "
+        ret += str(row['ip']) + ""
         ret += '<br>\n'
     conn.close()
     return navigation + ret
