a8551d5c833675e8220beef2c1b0f0556334f49b
N.F </dev/null>
2020-11-29 16:38:48 -0600

    Hack it to functionality

--- pypoll.py
+++ pypoll.py
@@ -14,7 +14,7 @@ navigation = '''
     <ul>
         <li> <a href="/"      >/</a>       </li>
         <li> <a href="/poll"  >/poll</a>   </li>
-        <li> <a href="/add-q"  >/add-q</a> </li>
+        <li> <a href="/add-p"  >/add-p</a> </li>
         <li> <a href="/insert">/insert</a> </li>
         <li> <a href="/select">/select</a> </li>
     </ul>
@@ -79,26 +79,99 @@ def authorized_row(token):
 def home():
     return navigation
 
+def get_user_id(request, session, makenew=True):
+    c = get_db().cursor()
+    u_row = None
+    if 'unique_id' in session:
+        u_row = c.execute("SELECT id FROM users WHERE cookie=?",(session['unique_id'], )).fetchone()
+    else:
+        session['unique_id'] = make_unique_id()
+    if u_row:
+        print("users row=" + str(u_row['id']) + " retreved")
+        return u_row['id']
+    if not makenew:
+        return None
+    now = time.time()
+    values = (now, session['unique_id'], request.remote_addr)
+    c.execute("INSERT INTO users (date, cookie, ip) VALUES (?, ?, ?)", values)
+    print("users row=" + str(c.lastrowid) + " created")
+    return c.lastrowid
+
+def get_next_question_id(poll, ques):
+    #TODO
+    return
+@app.route('/thanks')
+def poll_end():
+    conn = get_db()
+    c = conn.cursor()
+    p_id = request.args.get('poll')
+    if not get_user_id(request, session, makenew=False):
+        return redirect(url_for('home'))
+    ret = ""
+    ret += "<h2>Thanks for your time</h2>"
+    ret += '<a href="' + url_for('home') + '">back</a>'
+    return ret
+
+
+
+
 @app.route('/poll', methods=['GET', 'POST'])
 def poll_test():
+    conn = get_db()
+    c = conn.cursor()
+    p_id = request.args.get('poll')
+    q_id = request.args.get('ques')
     if request.method == 'POST':
-        if 'unique_id' in session:
-            return 'thonk'
-        session['unique_id'] = make_unique_id()
-        now = time.time()
-        conn = get_db()
-        c = conn.cursor()
-        values = (now, session['unique_id'], request.remote_addr)
-        c.execute("INSERT INTO users (date, cookie, ip) VALUES (?, ?, ?)", values)
+        u_id = get_user_id(request, session)
+        if 'answer' not in request.form:
+            return redirect(url_for('poll_test', poll=p_id, ques=q_id))
+        a_id = request.form['answer']
+        vals = (u_id, q_id, a_id)
+        answered_before = c.execute('SELECT id FROM user_answers WHERE user_id=? AND question_id=?',(u_id, q_id)).fetchone()
+        if not answered_before:
+            print("new")
+            c.execute('INSERT INTO user_answers (user_id, question_id, answer_id) VALUES (?,?,?)', vals)
+        else:
+            print("old")
+        ret_next = False
+        for question in c.execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id, )):
+            #print(tuple(question))
+            if ret_next:
+                return redirect(url_for('poll_test', poll=p_id, ques=question['id']))
+            print(type(q_id))
+            print(type(question['id']))
+            if question['id'] == int(q_id):
+                ret_next = True
+        if ret_next:
+            return redirect(url_for('poll_end', poll=p_id))
 
+        return redirect(url_for('poll_test', poll=p_id, ques=q_id))
 
-        return redirect(url_for('home'))
-    return '''
-    <form method="post">
-    <p><input type=text name=username>
+    poll_name = c.execute("SELECT name FROM poll WHERE id=?", (p_id, )).fetchone()['name']
+    row = None
+    if q_id:
+        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=? AND id=?", (p_id, q_id)).fetchone()
+    else:
+        #TODO: proper data ordering
+        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id,)).fetchone()
+        q_id = c.lastrowid
+    ret = "qid " + str(row['id']) + "<br>"
+    ret += "question: " + row['question'] + "<br>"
+    ret += '<form method="post" action="' + url_for('poll_test',poll=p_id,ques=str(row['id'])) + '">'
+    for r in c.execute("SELECT * FROM radio_question_answers WHERE radio_question_id=?", (row['id'], )):
+        ret += '<p><input type="radio" id="' + str(r['id']) + '" name="answer" value="' + str(r['id']) + '">' + str(r['answer']) +'</p>'
+    ret += '''
     <p><input type=submit name=whatever>
     </form>
     '''
+    #ret += '<form method="post" action="' + url_for('poll_test',poll=p_id,ques=q_id) + '">'
+    #ret += '''
+    #<p><input type=text name=username>
+    #'''
+    #ret += '<input type=submit name=whatever action="' + url_for('poll_test',poll=p_id,ques=q_id) + '">'
+    #ret += '</form>'
+    return ret
+
 
 @app.route('/add-p', methods=['GET', 'POST'])
 def add_poll():
@@ -188,14 +261,28 @@ def poll_status():
     p_id = authorized_row(token)
     if not p_id:
         return redirect(url_for('home'))
+    p_url = url_for('poll_test', poll=p_id)
+    s_url = url_for('poll_status', auth=token)
     ret = ""
+    ret += "<h2>Poll status page</h2>"
+    ret += "<p>poll url:<br><a href=" + p_url + ">" + p_url + "</a></p><br>"
+    ret += "<p>poll results url(keep this safe):<br><a href=" + s_url + ">" + s_url + "</a></p><br>"
     print(type(p_id))
     for que in query_db('SELECT * FROM radio_questions WHERE poll_id=?', str(p_id)):
-        ret += que['question'] + "<br>"
-        for user_ans in query_db('SELECT * FROM user_answers WHERE question_id=?', str(que['id'])):
-            ret += "user" + str(tuple(user_ans)) + "<br>"
+        ret += 'id '+str(que['id'])+' question: "' + que['question'] + '"'
+        ret += '<ul>'
         for poss_ans in query_db('SELECT * FROM radio_question_answers WHERE radio_question_id=?', str(que['id'])):
-            ret += "poss" + str(tuple(poss_ans)) + "<br>"
+            ret += "<li>id "+str(poss_ans['id'])+' answer: "' + (poss_ans['answer']) + '"'
+            count = 0
+            for user_ans in query_db('SELECT * FROM user_answers WHERE question_id=? AND answer_id=?', (str(que['id']), poss_ans['id'])):
+                count += 1
+            ret += " tally: " + str(count) + "<ul>"
+            for user_ans in query_db('SELECT * FROM user_answers WHERE question_id=? AND answer_id=?', (str(que['id']), poss_ans['id'])):
+                u = query_db('SELECT * FROM users WHERE id=?', (user_ans['user_id'], )).fetchone()
+                token = base64.b64encode(u['cookie']).decode()
+                ret += "<li>" +u['ip']  + " " + token + "</li>"
+            ret += "</ul></li>"
+        ret += '</ul>'
     return ret
 
 
