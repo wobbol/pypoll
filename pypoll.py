380b9d8c8b918883f880272593362d49901ce410
N.F </dev/null>
2020-12-08 12:52:49 -0600

    Move init to app.before_first_request save secret key

--- pypoll.py
+++ pypoll.py
@@ -5,11 +5,48 @@ import base64
 from flask import Flask, session, redirect, url_for, request, g
 from socket import inet_pton, inet_ntop
 from struct import unpack, pack
-app = Flask(__name__)
-app.secret_key = os.urandom(16) # TODO: store this in a file
 
+app = Flask(__name__)
+secretfile = 'secret'
 database = 'data.db'
 urlbase = '/python-poll'
+
+@app.before_first_request
+def before_first_request():
+    #init secretfile
+    header = b'SKEY'
+    skey_len = 16
+    try:
+        with open(secretfile, 'rb') as f:
+            f.seek(0, os.SEEK_END)
+            if f.tell() == 0:
+                raise IOError
+            if f.tell() != (len(header) + skey_len):
+                raise # file isn't ours.
+            f.seek(0, os.SEEK_SET)
+            if header != f.read(len(header)):
+                raise # file isn't ours.
+            app.secret_key = f.read(skey_len)
+            if (skey_len + len(header)) != f.tell():
+                raise IOError
+    except IOError:
+        with open(secretfile, 'wb') as f:
+            app.secret_key = os.urandom(skey_len)
+            f.write(header)
+            f.write(app.secret_key)
+
+    # init database
+    with app.app_context():
+        try:
+            f = open(database)
+            f.close()
+        except IOError:
+            conn = get_db()
+            with app.open_resource('tables.sql', mode='r') as t:
+                conn.cursor().executescript(t.read())
+                # if this fails it will raise sqlite3.OperationalError
+            conn.commit()
+
 def navigation():
     n = ""
     n += "<h3>Navigation</h3>"
@@ -51,13 +88,6 @@ def get_db():
         db.execute('PRAGMA foreign_keys = 1')
     return db
 
-def create_tables():
-    conn = get_db()
-    with app.open_resource('tables.sql', mode='r') as t:
-        conn.cursor().executescript(t.read())
-        # if this fails it will raise sqlite3.OperationalError
-    conn.commit()
-
 def query_db(query, args=()):
     return get_db().execute(query, args)
 
@@ -67,6 +97,7 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+#TODO: make a better name
 def authorized_row(token):
     count = 0
     rowid = ()
@@ -84,6 +115,7 @@ def home():
     ret += navigation()
     return ret
 
+#TODO: maybe remove get from this name
 def get_user_id(request, session, makenew=True):
     c = get_db().cursor()
     u_row = None
@@ -117,50 +149,80 @@ def poll_end():
     ret += '<a href="' + url_for('home') + '">back</a>'
     return ret
 
+def user_answered_before(u_id, q_id):
+    user_ans = get_db().cursor().execute('SELECT id FROM user_answers WHERE user_id=? AND question_id=?', (u_id, q_id))
+    return user_ans.fetchone()
+
+def insert_user_answer(u_id, q_id, a_id):
+    vals = (u_id, q_id, a_id)
+    return get_db().cursor().execute('INSERT INTO user_answers (user_id, question_id, answer_id) VALUES (?,?,?)', vals)
+
+def question_list(p_id):
+        return get_db().cursor().execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id, ))
+
+def poll_name(p_id):
+    return get_db().cursor().execute("SELECT name FROM poll WHERE id=?", (p_id, )).fetchone()['name']
+
+def poll_submit():
+    p_id = request.args.get('poll')
+    q_id = request.args.get('ques')
+    u_id = get_user_id(request, session)
+    if 'answer' not in request.form:
+        return redirect(url_for('poll_test', poll=p_id, ques=q_id))
+    a_id = request.form['answer']
+    if not user_answered_before(u_id, q_id):
+        print("new")
+        insert_user_answer(u_id, q_id, a_id)
+    else:
+        print("old")
+    ret_next = False
+    for question in query_db("SELECT * FROM radio_questions WHERE poll_id=?", (p_id, )):
+        #TODO: make sure to get these rows in order
+        #print(tuple(question))
+        if ret_next:
+            return redirect(url_for('poll_test', poll=p_id, ques=question['id']))
+        print(type(q_id))
+        print(type(question['id']))
+        if question['id'] == int(q_id):
+            ret_next = True
+    if ret_next:
+        return redirect(url_for('poll_end', poll=p_id))
+
+    return redirect(url_for('poll_test', poll=p_id, ques=q_id))
+
+def get_question(p_id, q_id):
+    row = None
+    c = get_db().cursor()
+    if q_id:
+        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=? AND id=?", (p_id, q_id)).fetchone()
+    else:
+        # TODO: make sure to get these rows in order
+        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id,)).fetchone()
+        q_id = c.lastrowid
+    return row
+
+def question_answers_list(q_id):
+    return get_db().cursor().execute("SELECT * FROM radio_question_answers WHERE radio_question_id=?", (q_id, ))
+
 @app.route(urlbase + '/poll', methods=['GET', 'POST'])
 def poll_test():
     conn = get_db()
     c = conn.cursor()
+    if request.method == 'POST':
+        return poll_submit()
+
     p_id = request.args.get('poll')
     q_id = request.args.get('ques')
-    if request.method == 'POST':
-        u_id = get_user_id(request, session)
-        if 'answer' not in request.form:
-            return redirect(url_for('poll_test', poll=p_id, ques=q_id))
-        a_id = request.form['answer']
-        vals = (u_id, q_id, a_id)
-        answered_before = c.execute('SELECT id FROM user_answers WHERE user_id=? AND question_id=?',(u_id, q_id)).fetchone()
-        if not answered_before:
-            print("new")
-            c.execute('INSERT INTO user_answers (user_id, question_id, answer_id) VALUES (?,?,?)', vals)
-        else:
-            print("old")
-        ret_next = False
-        for question in c.execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id, )):
-            #print(tuple(question))
-            if ret_next:
-                return redirect(url_for('poll_test', poll=p_id, ques=question['id']))
-            print(type(q_id))
-            print(type(question['id']))
-            if question['id'] == int(q_id):
-                ret_next = True
-        if ret_next:
-            return redirect(url_for('poll_end', poll=p_id))
+    u_id = get_user_id(request, session)
 
-        return redirect(url_for('poll_test', poll=p_id, ques=q_id))
+    p_name = poll_name(p_id) #XXX: unused
 
-    poll_name = c.execute("SELECT name FROM poll WHERE id=?", (p_id, )).fetchone()['name']
-    row = None
-    if q_id:
-        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=? AND id=?", (p_id, q_id)).fetchone()
-    else:
-        #TODO: proper data ordering
-        row = c.execute("SELECT * FROM radio_questions WHERE poll_id=?", (p_id,)).fetchone()
-        q_id = c.lastrowid
+    row = get_question(p_id, q_id)
+
     ret = "qid " + str(row['id']) + "<br>"
     ret += "question: " + row['question'] + "<br>"
     ret += '<form method="post" action="' + url_for('poll_test',poll=p_id,ques=str(row['id'])) + '">'
-    for r in c.execute("SELECT * FROM radio_question_answers WHERE radio_question_id=?", (row['id'], )):
+    for r in question_answers_list(row['id']):
         ret += '<p><input type="radio" id="' + str(r['id']) + '" name="answer" value="' + str(r['id']) + '">' + str(r['answer']) +'</p>'
     ret += '''
     <p><input type=submit name=whatever>
@@ -213,7 +275,7 @@ def add_answer():
     if not authorized_row(token):
         return redirect(url_for('home'))
     q_id = request.args.get('quest', '')
-    question = query_db('SELECT * from radio_questions WHERE id=?', q_id)
+    question = query_db('SELECT * from radio_questions WHERE id=?', (q_id, ))
     # if no row redirect to add_question()
     if request.method == 'POST':
         conn = get_db()
@@ -236,7 +298,7 @@ def add_answer():
     ret += 'Question: "'
     ret += question.fetchone()['question']
     ret += '"<br>'
-    for row in query_db('SELECT answer FROM radio_question_answers WHERE radio_question_id=?', q_id):
+    for row in query_db('SELECT answer FROM radio_question_answers WHERE radio_question_id=?', (q_id, )):
         ret += 'q '
         ret += row['answer']
         ret += '<br>'
@@ -266,7 +328,7 @@ def poll_status():
     for que in query_db('SELECT * FROM radio_questions WHERE poll_id=?', str(p_id)):
         ret += 'id '+str(que['id'])+' question: "' + que['question'] + '"'
         ret += '<ul>'
-        for poss_ans in query_db('SELECT * FROM radio_question_answers WHERE radio_question_id=?', str(que['id'])):
+        for poss_ans in query_db('SELECT * FROM radio_question_answers WHERE radio_question_id=?', (str(que['id']), )):
             ret += "<li>id "+str(poss_ans['id'])+' answer: "' + (poss_ans['answer']) + '"'
             count = 0
             for user_ans in query_db('SELECT * FROM user_answers WHERE question_id=? AND answer_id=?', (str(que['id']), poss_ans['id'])):
--- pypoll.sh
+++ pypoll.sh
@@ -5,17 +5,5 @@ DIR=$(dirname $0)
 source "$DIR/venv/bin/activate"
 export FLASK_APP="$DIR/pypoll.py"
 export FLASK_ENV=development
-cd "$DIR"
-python3 << EOF
-import pypoll
-f = None
-try:
-    f = open(pypoll.database)
-    f.close()
-except IOError:
-    with pypoll.app.app_context():
-        pypoll.create_tables()
-EOF
-cd -
 flask run
 
